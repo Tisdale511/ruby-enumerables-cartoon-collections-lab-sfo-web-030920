b7d38148425ca2bb70260b65293227b04a9f6cf5
Tisdale Fry <tisdalefry@gmail.com>
2020-03-05 18:49:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,8 @@
 def roll_call_dwarves(names) # code an argument here
   # Your code here
   names.each_with_index do |name, idx|
-    puts "#{idx} #{name}\n"
+    puts "#{idx} #{name}"
+    puts
   end
 end
 
