269692b5da20b1654df1f206adc7b2fcf5a28a98
Tisdale Fry <tisdalefry@gmail.com>
2020-03-05 19:55:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types do |cheese|
+  cheese_types.each do |cheese|
     if array.include?(cheese)
       return cheese
     end
