0c796e56d2cf399e63d1a2c116d35ec0edd92b3f
Tisdale Fry <tisdalefry@gmail.com>
2020-03-05 18:36:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names) # code an argument here
   # Your code here
+  array.each_with_index do |name, idx|
+    puts "#{idx} #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
